2bcdec1b39fdec5f810ea600d071f122800bec6b
陆凌风 <lulingfengweb@gmail.com>
2025-09-02 18:54:04 +0800

    dev

--- src/App.vue
+++ src/App.vue
@@ -24,26 +24,26 @@ async function checkForUpdates() {
     console.log(`output->update`,update)
     if (update) {
       console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
-      let downloaded = 0;
-      let contentLength = 0;
+      // let downloaded = 0;
+      // let contentLength = 0;
       // alternatively we could also call update.download() and update.install() separately
-      await update.downloadAndInstall((event) => {
-        switch (event.event) {
-          case 'Started':
-            contentLength = event.data.contentLength ?? 0;
-            console.log(`started downloading ${event.data.contentLength} bytes`);
-            break;
-          case 'Progress':
-            downloaded += event.data.chunkLength;
-            console.log(`downloaded ${downloaded} from ${contentLength}`);
-            break;
-          case 'Finished':
-            console.log('download finished');
-            break;
-        }
-      });
+      // await update.downloadAndInstall((event) => {
+      //   switch (event.event) {
+      //     case 'Started':
+      //       contentLength = event.data.contentLength ?? 0;
+      //       console.log(`started downloading ${event.data.contentLength} bytes`);
+      //       break;
+      //     case 'Progress':
+      //       downloaded += event.data.chunkLength;
+      //       console.log(`downloaded ${downloaded} from ${contentLength}`);
+      //       break;
+      //     case 'Finished':
+      //       console.log('download finished');
+      //       break;
+      //   }
+      // });
 
-      console.log('update installed');
+      // console.log('update installed');
       await relaunch();
     }
     updateStatus.value = '更新检查完成';
